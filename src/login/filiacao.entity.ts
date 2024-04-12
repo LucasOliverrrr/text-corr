57a8d496da0ae7676611cfb3cfacbc880a9e3c36
LucasOliverrrr <lucasouze@gmail.com>
2024-04-12 11:49:31 -0300

    Carmino

---
+++ src/login/filiacao.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {Login} from "./login.entity"
+@Entity()
+export class Filiacao {
+    @PrimaryGeneratedColumn()
+    id_filiacao: number;
+    
+    @Column({ length: 40 })
+    nome: string;
+
+    @Column({ length: 40 })
+    endereco: string;
+
+    @Column({length: 30})
+    bairro: string;
+
+    @Column({length: 2})
+    uf: string;
+
+    @ManyToOne(() => Login, (Login) => Login.filiacao)
+    @JoinColumn({name:"id_login"})
+    login: Login;
+}
---
+++ src/login/login.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Filiacao } from "./filiacao.entity";
+
+@Entity()
+export class Login {
+    @PrimaryGeneratedColumn()
+    id_login: number;
+    
+    @Column({ unique: true, length: 40 })
+    email: string;
+
+    @Column({ length: 60 })
+    senha: string;
+
+    @Column({length: 40})
+    nome: string;
+
+    @OneToMany(() => Filiacao, (filiacao) => filiacao.login)
+    filiacao: Filiacao[];
+}
